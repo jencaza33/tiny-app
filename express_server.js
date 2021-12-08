7c5461428c16378ef376bdeeaff8d4c64eef76c9
Jen Caza <jencaza@gmail.com>
2021-12-07 20:02:39 -0500

    Add missing </form> on /urls_show.ejs, add POST route

--- express_server.js
+++ express_server.js
@@ -79,6 +79,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.newURL;
+  res.redirect('/urls');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
