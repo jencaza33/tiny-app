eddd7c3a3c087b0ab066d965a2c667080e99802a
Jen Caza <jencaza@gmail.com>
2021-12-07 20:45:06 -0500

    Add logic to differentiate if user logged in, add route for /logout to remove username cookie

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
@@ -18,11 +19,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -32,20 +31,35 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 /* Responds to '/urls' GET request with rendered HTML of urls_index.ejs. */
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render('urls_index', templateVars);
 });
+
 /* Responds to '/urls/new' GET request with rendered HTML of urls_new.ejs */
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
+
 /* Responds to '/urls/:shortURL' GET request with rendered HTML of urls_show.ejs with data specific to :shortURL */
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
+
 /* Responds to '/u/:shortURL' GET request with the corresponding long URL, from the urlDatabase */
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
@@ -72,10 +86,14 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[shortURL] = req.body.newURL;
   res.redirect('/urls');
 });
-
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect('/urls');
 });
 
